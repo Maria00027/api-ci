edbe9b1bf380a3e98cc818859d5a4b3d1c1907a2
Maria00027 <luma000.27@gmail.com>
2022-09-18 12:34:11 +0300

    fix schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -25,6 +25,8 @@ class MobileBankApiTestV3 {
           .body("", hasSize(3))
           .body("[0].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0))
+              .body("[1].currency", equalTo("USD"))
+              .body("[1].balance", greaterThanOrEqualTo(0))
       ;
     }
 }
